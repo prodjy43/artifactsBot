bae84710bad837c032d67f351920ba9aa89ecae2
Neoncamouflage <neoncamouflage@yahoo.com>
2024-07-30 16:51:35 -0700

    Objective and task system set

--- src/Character.js
+++ src/Character.js
@@ -1,14 +1,20 @@
 const gameAPI = require('./gameAPI');
 const {AUTH_TOKEN,SKILLS,INVENTORY_SLOTS}  = require('./constants');
+const jobNameRef = {
+    'Amos':'miner',
+    'Philip':'woodcutter'
+}
 
 //Character prototype to handle actions and tracking information
 function Character(data) {
     this.authorization = AUTH_TOKEN;
     this.skills = {};
-    this.inventory = [];
+    this.inventory = {};
     this.task = {};
     this.gear = {};
-    this.objective = {};
+    this.objective = null;
+    this.jobs = [];
+    this.personality = Math.floor(Math.random() * (500 - 100 + 1)) + 100;
     this.setData(data);
 }
 
@@ -71,11 +77,12 @@ Character.prototype.setData = function(data){
     //Set up inventory array with item objects
     for(let i = 1; i <= INVENTORY_SLOTS; i++){
         let key = `inventory_slot${i}`;
-        this.inventory.push({
+        if(!data[key]) continue;
+        this.inventory[data[key]] = {
             slot:i,
-            itemName:data[key],
+            item:data[key],
             quantity:data[key+'_quantity']
-        })
+        }
     }
 }
 
@@ -90,13 +97,147 @@ Character.prototype.setData = function(data){
  * 2. Be at full health
  * 3. Complete Task
  */
-Character.prototype.getObjective = function(){
-    //See if we need better gear
-    for(let [slot,item] of Object.entries(this.gear)){
-        //If we're missing an item
-        if(!item){
+Character.prototype.getObjective = function(gameObjects){
+    //Shuffle gear objective until we have a better way to decide
+    let gearKeys = Object.keys(this.gear)
+    let currentIndex = gearKeys.length;
 
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [gearKeys[currentIndex], gearKeys[randomIndex]] = [
+            gearKeys[randomIndex], gearKeys[currentIndex]];
+    }
+
+    //See if we need better gear
+    for(let slot of gearKeys){
+        let item = this.gear[slot]
+        //If we're missing an item, our goal is to fill that item slot if we can
+        //Extra check for the starter stick because it's awful
+        if(!item || item.code == 'wooden_stick'){
+            //Set the specific type we need, to make sure we get the rings as well
+            let typeNeeded = ['ring1','ring2'].includes(slot) ? 'ring' : slot;
+            //Create an array for all viable objectives for this slot
+            let itemOptions = [];
+            for(let item of Object.values(gameObjects['items'])){
+                //Skip if the wrong type or too high a level
+                if(item.type != typeNeeded) continue;
+                if(item.level > this.level) continue;
+                //If it's valid, check if we already have one and equip if so
+                if(this.inventory[item.code]){
+                    this.equip(item.code);
+                    itemOptions = [];
+                    break;
+                }
+                //If not, add it to the list of options
+                itemOptions.push(item);
+            }
+            //If we didn't find any options, move on
+            if(!itemOptions.length) continue;
+            //Otherwise, pick a random one and set it as our objective
+            let pick = itemOptions[Math.floor(Math.random() * itemOptions.length)];
+            this.objective = {
+                type:'get',
+                target:pick,
+                quantity:1
+            };
+            return;
         }
+    }
+    //If we have all the items we need, make sure our health is full before moving on
+
+    //If our health is full, work on our task or get one if we need
+    this.objective = {
+        type:'get',
+        target:'task'
+    }
+}
+
+//Gets all subjobs that need done to fulfill the main job
+Character.prototype.getJobs = function(parentJob,gameObjects){
+    //console.log("Getting jobs!")
+    let {target, type, quantity} = parentJob;
+    let targetName = target.code
+    //Objective is to acquire something
+    if(type == 'get'){
+        //console.log("Job is to get something")
+        //If we already have the item, then no job is needed
+        if(this.inventory[targetName] && this.inventory[targetName].quantity >= quantity){
+            //console.log("Already got it!")
+            return;
+        }
+        //See if it's a craftable item
+        if(target.craft && target.craft.skill){
+            //console.log("Crafting job!")
+            //Add a craft job
+            let craftJob = {
+                type:'craft',
+                target:target,
+                quantity:quantity
+            }
+            this.jobs.unshift(craftJob);
+            //Add all components as get jobs
+            for(let component of target.craft.items){
+                //console.log("Adding",JSON.stringify(component))
+                let getJob = {
+                    type:'get',
+                    target:gameObjects.items[component.code],
+                    quantity:component.quantity * quantity //We need components for each unit in the parent job
+                }
+                //console.log("Inserting",JSON.stringify(getJob))
+                this.jobs.unshift(getJob);
+                //Get all subjobs for the new job
+                this.getJobs(getJob,gameObjects);
+            }
+            return;
+        }
+        //If not, see if it's a resource we need to gather
+        if(target.type == 'resource'){
+            //console.log("Get a resource")
+            //See if it's a monster drop by checking for the subtype in skills
+            if(!SKILLS.includes(target.subtype)){
+                //console.log("Monster drop!")
+                //Find the monster that gives our drop
+                let targetMonster;
+                let droprate = 0;
+                for(let monster of Object.values(gameObjects.monsters)){
+                    for(let drop of monster.drops){
+                        if(drop.code == targetName){
+                            droprate = drop.rate;
+                            targetMonster = monster;
+                        }
+                    }
+                }
+                this.jobs.unshift({
+                    type:'kill',
+                    target:targetMonster,
+                    quantity:quantity*droprate
+                });
+            }
+            //If not a monster drop, add a harvest job
+            this.jobs.unshift({
+                type:'harvest',
+                target:target,
+                quantity:quantity
+            });
+            //See if our skill is high enough to gather it, return if so
+
+            if(target.level <= this.skills[target.subtype].level) return;
+            //If it isn't, add a training job
+            //Add a harvest job
+            this.jobs.unshift({
+                type:'train',
+                target:target.subtype,
+                quantity:target.level
+            });
+            return;
+        }
+        console.log("End of job check for",JSON.stringify(parentJob))
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,27 +1,43 @@
 const Character = require('./Character');
 const Game = require('./Game');
 
+//Primary objects
+let gameObjects = {};
+let gameMap = {};
+let characters;
 
-
-function gameLoop() {
-    setInterval(() => {
-        characters.forEach(character => {
-            if (Math.random() > 0.5) {
-                randomMove(character);
-            } else {
-                randomFight(character);
+//Primary game loop
+async function gameLoop() {
+    while (true) {
+        //Perform actions for each character
+        let chCount = 1;
+        for(let char of characters){
+            if(char.cooldown) continue;
+            if(!char.objective) char.getObjective(gameObjects);
+            if(!char.jobs || !char.jobs.length) char.getJobs(char.objective,gameObjects);
+            if(chCount > 1) console.log('-------------------------------------------------------------------------------')
+            console.log(`${char.name} has primary objective: ${char.objective.type} ${char.objective.quantity} ${char.objective.target.name}`);
+            console.log("Subtasks:");
+            for (let i = 0; i <= char.jobs.length - 1; i++) {
+                let job = char.jobs[i];
+                console.log(`Job ${i+1}: ${job.type} ${job.quantity} ${job.target.name}`);
             }
-        });
-    }, 3000);
+            chCount++;
+        }
+        //Get minimum cooldown for our delay
+        let minCooldown = Math.min(...characters.map(char => char.cooldown));
+        //console.log(`Minimum cooldown is ${minCooldown}`)
+        process.exit()
+        //Wait as long as we need for the cooldown
+        await new Promise(resolve => setTimeout(resolve, minCooldown));
+
+        console.log(`Waiting for ${minCooldown} ms before next iteration`);
+    }
 }
 
-
-
+//Startup logic that only needs run once, then calls the game loop
 async function main(){
     //Create our main reference for game objects and the game map
-    let gameObjects = {};
-    let gameMap = {};
-    let characters =[];
     //Get character data and create an array of Character instances for each
     let apiResponse = await Game.getMyCharacters();
     characters = apiResponse.data.map(data => new Character(data));
@@ -36,15 +52,14 @@ async function main(){
         let key = `${spot.x,spot.y}`;
         gameMap[key] = spot;
     }
-    characters[0].getObjective()
-    //gameLoop();
+    //Begin running the game loop
+    gameLoop();
 }
 
 function bucketByCode(list){
     let returnObj = {};
     for(let item of list){
-        returnObj[item.code] = returnObj[item.code] || [];
-        returnObj[item.code].push(item)
+        returnObj[item.code] = item;
     }
     return returnObj;
 }
